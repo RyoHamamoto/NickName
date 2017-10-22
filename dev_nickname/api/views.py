599b54072493f2e1c32c2101d952549824724916
Yuichi Kubo <newstart1ine.yk@gmail.com>
2017-10-22 10:48:52 +0000

    複数リクエスト対応追加

--- dev_nickname/api/views.py
+++ dev_nickname/api/views.py
@@ -8,20 +8,24 @@ from .character_converter import chara2romaji
 from .romaji_converter import romaji2hiragana
 import json
 
-def name(request):
-    if request.method == 'POST' and request.POST["name"]:
-        
-        # 送られてきたリクエストを全てローマ字変換
-        n = chara2romaji(request.POST["name"])
 
-        # 全てローマ字に変換された文字列をひらがなに変換
-        nickname = romaji2hiragana(n)
+def name(request):
+    if request.method == 'POST':
         
         # 配列を作成
         dic = []
         
-        # 配列にセット
-        dic = [("name", nickname + "さん")]
+        for key in request.POST:
+        
+         # 送られてきたリクエストを全てローマ字変換
+         n = chara2romaji(request.POST[key])
+
+         # 全てローマ字に変換された文字列をひらがなに変換
+         nickname = romaji2hiragana(n)
+         
+         # 配列にセット
+         dic.append({key:nickname + "さん"})
+        
         
         # json形式に変換
         result = json.dumps(dic, ensure_ascii=False)
@@ -30,11 +34,7 @@ def name(request):
         response = HttpResponse(content=result, content_type='application/json; charset=UTF-8', status=200)
         return response
         
-        
-        # return render(request, 'name.html', {'namae': response})
     else:
-        # response = HttpResponseNotFound()
-        # return resopnse
         
         # フォーム表示
          form = NameForm()
