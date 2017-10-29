a7b0912bc742cb58eb6263ac23aa682842e7f5c9
Yuichi Kubo <newstart1ine.yk@gmail.com>
2017-10-29 22:50:14 +0000

    空配列空のまま返す処理追加+post中身をキーでソートする処理追加

--- dev_nickname/api/views.py
+++ dev_nickname/api/views.py
@@ -7,34 +7,38 @@ from .forms import NameForm
 from .character_converter import chara2romaji
 from .romaji_converter import romaji2hiragana
 from .romaji_converter import romaji2katakana
+from collections import OrderedDict
 import json
 import mojimoji
 
 
 def hiragana(request):
+    print(request.POST)
     if request.method == 'POST':
         
         # 配列を作成
-        dic = []
-        
-        for key in request.POST:
-         
-         # アルファベット以外半角を全角へ変換する処理
-         nam  = mojimoji.zen_to_han(request.POST[key])
-         name = mojimoji.han_to_zen(nam, ascii=False)
-        
-         # 送られてきたリクエストを全てローマ字変換
-         n = chara2romaji(name)
+        od = OrderedDict()
 
-         # 全てローマ字に変換された文字列をひらがなに変換
-         nickname = romaji2hiragana(n)
+        for key in sorted(request.POST):
          
-         # 配列にセット
-         dic.append({key:nickname + "さん"})
-        
-        
+            # アルファベット以外半角を全角へ変換する処理
+            nam  = mojimoji.zen_to_han(request.POST[key])
+            name = mojimoji.han_to_zen(nam, ascii=False)
+            
+            # 送られてきたリクエストを全てローマ字変換
+            n = chara2romaji(name)
+    
+            # 全てローマ字に変換された文字列をひらがなに変換
+            nickname = romaji2hiragana(n)
+             
+            # 配列にセット
+            if nickname != "":
+                od[key] = nickname + "さん"
+            else:
+                od[key] = nickname
+            
         # json形式に変換
-        result = json.dumps(dic, ensure_ascii=False)
+        result = json.dumps(od, ensure_ascii=False)
         
         # 結果を返す
         response = HttpResponse(content=result, content_type='application/json; charset=UTF-8', status=200)
@@ -43,38 +47,41 @@ def hiragana(request):
         response["Access-Control-Max-Age"] = "1000"
         response["Access-Control-Allow-Headers"] = "*"
         return response
-        
+    
     else:
         
         # フォーム表示
-         form = NameForm()
-         return render(request, 'name.html', {'form': form})
+        form = NameForm()
+        return render(request, 'name.html', {'form': form})
 
 
 def katakana(request):
     if request.method == 'POST':
         
         # 配列を作成
-        dic = []
+        od = OrderedDict()
         
-        for key in request.POST:
+        for key in sorted(request.POST):
         
-         # アルファベット以外半角を全角へ変換する処理
-         nam  = mojimoji.zen_to_han(request.POST[key])
-         name = mojimoji.han_to_zen(nam, ascii=False)
-        
-         # 送られてきたリクエストを全てローマ字変換
-         n = chara2romaji(name)
-
-         # 全てローマ字に変換された文字列をカタカナに変換
-         nickname = romaji2katakana(n)
-         
-         # 配列にセット
-         dic.append({key:nickname + "さん"})
+            # アルファベット以外半角を全角へ変換する処理
+            nam  = mojimoji.zen_to_han(request.POST[key])
+            name = mojimoji.han_to_zen(nam, ascii=False)
+            
+            # 送られてきたリクエストを全てローマ字変換
+            n = chara2romaji(name)
+    
+            # 全てローマ字に変換された文字列をカタカナに変換
+            nickname = romaji2katakana(n)
+             
+             # 配列にセット
+            if nickname != "":
+                od[key] = nickname + "さん"
+            else:
+                od[key] = nickname
         
         
         # json形式に変換
-        result = json.dumps(dic, ensure_ascii=False)
+        result = json.dumps(od, ensure_ascii=False)
         
         # 結果を返す
         response = HttpResponse(content=result, content_type='application/json; charset=UTF-8', status=200)
@@ -87,5 +94,5 @@ def katakana(request):
     else:
         
         # フォーム表示
-         form = NameForm()
-         return render(request, 'name.html', {'form': form})
+        form = NameForm()
+        return render(request, 'name.html', {'form': form})
